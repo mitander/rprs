e10aa25a9989fec3802f2f2b447aa10ef3a5ae4c
mitander <carlmitander@gmail.com>
2024-10-11 14:53:59 +0200

    add proxy and server args

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,53 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Request, Response, Server, Uri};
 use hyper_tls::HttpsConnector;
 use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    proxy_addr: SocketAddr,
+    #[arg(short, long)]
+    server_addr: SocketAddr,
+}
 
 #[tokio::main]
 async fn main() {
-    let proxy_addr = ([127, 0, 0, 1], 3000).into();
-    let make_service =
-        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
-    let server = Server::bind(&proxy_addr).serve(make_service);
+    let args = Args::parse();
+    let server_addr = Arc::new(args.server_addr);
+    let make_service = make_service_fn(move |_conn| {
+        let server_addr = Arc::clone(&server_addr);
 
-    println!("Reverse proxy listening on http://{}", proxy_addr);
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                let server_addr = Arc::clone(&server_addr);
+                handle_request(req, server_addr)
+            }))
+        }
+    });
+    let server = Server::bind(&args.proxy_addr).serve(make_service);
+
+    println!("Reverse proxy listening on http://{}", args.proxy_addr);
 
     if let Err(e) = server.await {
         eprintln!("Server error: {}", e);
     }
 }
 
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn handle_request(
+    req: Request<Body>,
+    server_addr: Arc<SocketAddr>,
+) -> Result<Response<Body>, Infallible> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
 
     let uri_string = format!(
-        "http://127.0.0.1:9001{}",
+        "http://{}{}",
+        server_addr,
         req.uri()
             .path_and_query()
             .map(|x| x.as_str())
